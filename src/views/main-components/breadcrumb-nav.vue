7385ba7ca83c9255243009f7cb6125830181d237
misCC <1518265554@qq.com>
2018-07-19 11:28:33 +0800

    up

--- src/views/main-components/breadcrumb-nav.vue
+++ src/views/main-components/breadcrumb-nav.vue
@@ -1,28 +1,29 @@
 <template>
-    <Breadcrumb>
-        <BreadcrumbItem 
-            v-for="item in currentPath" 
-            :href="item.path" 
-            :key="item.name"
-        >{{ itemTitle(item) }}</BreadcrumbItem>
-    </Breadcrumb>
+  <Breadcrumb>
+    <BreadcrumbItem
+        v-for="item in currentPath"
+        :href="item.path"
+        :key="item.name"
+    >{{ itemTitle(item) }}
+    </BreadcrumbItem>
+  </Breadcrumb>
 </template>
 
 <script>
-export default {
+  export default {
     name: 'breadcrumbNav',
     props: {
-        currentPath: Array
+      currentPath: Array
     },
     methods: {
-        itemTitle (item) {
-            if (typeof item.title === 'object') {
-                return this.$t(item.title.i18n);
-            } else {
-                return item.title;
-            }
+      itemTitle (item) {
+        if (typeof item.title === 'object') {
+          return this.$t(item.title.i18n);
+        } else {
+          return item.title;
         }
+      }
     }
-};
+  };
 </script>
 

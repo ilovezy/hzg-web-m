3d331fbc5f92fc6817f24eef7b9e2ac5fca6d2a5
misCC <1518265554@qq.com>
2018-07-20 18:22:49 +0800

    up

--- src/global/AXIOS.js
+++ src/global/AXIOS.js
@@ -4,66 +4,11 @@ import '../global/config'
 import {router} from '../router/index'
 import {Notice, Message} from 'iview'
 
-// const ajaxUrl = CONFIG.server[env] || 'https://debug.url.com';
-//
-// let AXIOS = axios.create({
-//   baseURL: ajaxUrl,
-//   // timeout: 30000
-//   timeout: 20000
-// })
-//
-// // AXIOS.defaults.transformRequest = [(queryParam) => {
-// //   // Do whatever you want to transform the data
-// //   let ret = '';
-// //   Object.keys(queryParam).forEach(it => {
-// //     ret += `${encodeURIComponent(it)}=${encodeURIComponent(queryParam[it])}&`;
-// //   })
-// //   return ret;
-// // }];
-//
-// AXIOS.interceptors.request.use(
-//   config => {
-//     config.data = JSON.stringify(config.data);
-//     config.headers = {
-//       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-//     }
-//     return config;
-//   },
-//   err => {
-//     return Promise.reject(err);
-//   }
-// );
-//
-// AXIOS.interceptors.response.use(res => {
-//   let data = res.data || {}
-//   if (data.isSuccess) {
-//     return data;
-//   } else {
-//     Notice.error({
-//       title: '提示',
-//       desc: data.errorDescription || '出现未知错误，请稍后重试',
-//       duration: 3
-//     })
-//   }
-//   return res;
-// }, (error) => {
-//   Notice.error({
-//     title: '提示',
-//     desc: '网络有点卡，请稍后重试',
-//     duration: 3
-//   })
-//   return Promise.reject(error);
-// })
-//
-// export default AXIOS;
-//
-
 let ajaxUrl = CONFIG.server[env] || 'https://debug.url.com'
 
 let instance = axios.create({
   baseURL: ajaxUrl,
-  // timeout: 30000
-  timeout: 20000
+  timeout: 10000
 })
 
 instance.interceptors.request.use(config => {
